129896c3f53f218b625101c66502c54fe959c23e
Richardsantos2K <richarddossantos2022@gmail.com>
2023-10-06 19:10:54 -0300

    Estudos api rest desde o basico ao avançado

--- src/main/java/com/bootcampsantander/apigames/entities/Game.java
+++ src/main/java/com/bootcampsantander/apigames/entities/Game.java
@@ -21,9 +21,15 @@ public class Game {
 
 
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")// Pelo texto ser acima de 255 caracteres devemos fazer isso para que a JPA aceite nossa descrição dos jogos, estamos definindo eles como tipo testo dentro do nosso Banco de dados
+
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -31,13 +37,14 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform,
-                String imgUrl, String shortDescription, String longDescription){
+    public Game(Long id, String title, Integer year, String genre, String platforms,
+                Double score, String imgUrl, String shortDescription, String longDescription){
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -76,12 +83,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
