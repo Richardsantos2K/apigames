0958a75d6de6635388e49fa873e120a7b9e8543f
Richardsantos2K <richarddossantos2022@gmail.com>
2023-10-06 18:22:50 -0300

    Estudos api rest desde o basico ao avançado

---
+++ src/main/java/com/bootcampsantander/apigames/ApigamesApplication.java
@@ -0,0 +1,13 @@
+package com.bootcampsantander.apigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcampsantander/apigames/ApigamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampsantander.apigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
